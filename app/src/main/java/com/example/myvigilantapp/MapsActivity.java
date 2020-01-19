0609f11e700ff22465b790578a5307fdf2e1e0a6
Michael <30552563+greenyellowblue@users.noreply.github.com>
2020-01-19 01:34:15 -0500

    Added Incident Report

--- app/src/main/java/com/example/myvigilantapp/MapsActivity.java
+++ app/src/main/java/com/example/myvigilantapp/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.myvigilantapp;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -9,6 +10,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -18,8 +20,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    // set radius to draw circles around event
+    private int radius = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -85,10 +91,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        float zoomLevel = 16.0f;
 
-        // Add a marker in Sydney and move the camera
+        // replace coordinates with coordinates of current location when we have that figured out
+        LatLng currentlocation = new LatLng(43.472944, -80.539775);
+
+        mMap.addMarker(new MarkerOptions().position(currentlocation).title("Your current location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentlocation, zoomLevel));
+        markLocation(43.470831, -80.541954);
+        markLocation(43.474350, -80.541690);
+
+        /* Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        */
+    }
+    private void markLocation(double x, double y){
+        LatLng newLocation = new LatLng(x, y);
+        mMap.addMarker(new MarkerOptions().position(newLocation).title("Police Reported Incident"));
+        drawCircle(new LatLng(x, y));
+    }
+    private void drawCircle(LatLng point){
+
+        // Instantiating CircleOptions to draw a circle around the marker
+        CircleOptions circleOptions = new CircleOptions();
+
+        // Specifying the center of the circle
+        circleOptions.center(point);
+
+        // Radius of the circle
+        circleOptions.radius(radius);
+
+        // Border color of the circle
+        circleOptions.strokeColor(Color.BLACK);
+
+        // Fill color of the circle
+        circleOptions.fillColor(0x30ff0000);
+
+        // Border width of the circle
+        circleOptions.strokeWidth(2);
+
+        // Adding the circle to the GoogleMap
+        mMap.addCircle(circleOptions);
+
     }
 }
