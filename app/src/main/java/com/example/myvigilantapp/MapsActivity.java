eb8f28058628b30c17361a391acd5ea2765fe562
Michael <30552563+greenyellowblue@users.noreply.github.com>
2020-01-19 03:03:41 -0500

    Added Location Services

--- app/src/main/java/com/example/myvigilantapp/MapsActivity.java
+++ app/src/main/java/com/example/myvigilantapp/MapsActivity.java
@@ -1,24 +1,37 @@
 package com.example.myvigilantapp;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import io.radar.sdk.Radar;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity<MainActivity> extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int REQUEST_CODE = 101;
 
     // set radius to draw circles around event
     private int radius = 100;
@@ -28,6 +41,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fetchLastLocation();
+        /*
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -76,6 +92,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // for permission.
 
         }
+        */
+
+    }
+
+    private void fetchLastLocation() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    currentLocation = location;
+                    Toast.makeText(getApplicationContext(), currentLocation.getLatitude()+""+currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    supportMapFragment.getMapAsync(MapsActivity.this);
+                }
+            }
+
+
+        });
     }
 
 
@@ -92,12 +132,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         float zoomLevel = 16.0f;
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
 
         // replace coordinates with coordinates of current location when we have that figured out
-        LatLng currentlocation = new LatLng(43.472944, -80.539775);
+        LatLng currentlocation = new LatLng(44, -81);
 
-        mMap.addMarker(new MarkerOptions().position(currentlocation).title("Your current location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentlocation, zoomLevel));
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Your current location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoomLevel));
+
         markLocation(43.470831, -80.541954);
         markLocation(43.474350, -80.541690);
 
@@ -107,6 +149,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         */
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case REQUEST_CODE:
+                if (grantResults.length>0 &&grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    fetchLastLocation();
+                }
+                break;
+        }
+
+    }
+
     private void markLocation(double x, double y){
         LatLng newLocation = new LatLng(x, y);
         mMap.addMarker(new MarkerOptions().position(newLocation).title("Police Reported Incident"));
